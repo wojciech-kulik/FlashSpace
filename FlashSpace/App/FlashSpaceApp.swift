bd25b2a52b38aab3f1eaf2460ce149ecaa262220
Wojciech Kulik <3128467+wojciech-kulik@users.noreply.github.com>
2025-02-13 21:08:07 +0100

    refactor: extracted FlashSpaceMenuBar (#135)

--- FlashSpace/App/FlashSpaceApp.swift
+++ FlashSpace/App/FlashSpaceApp.swift
@@ -10,10 +10,6 @@ import SwiftUI
 @main
 struct FlashSpaceApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    @Environment(\.openWindow) private var openWindow
-
-    @StateObject private var workspaceManager = AppDependencies.shared.workspaceManager
-    @StateObject private var settingsRepository = AppDependencies.shared.settingsRepository
 
     var body: some Scene {
         Window("⚡ FlashSpace v\(AppConstants.version)", id: "main") {
@@ -26,56 +22,6 @@ struct FlashSpaceApp: App {
         }
         .windowResizability(.contentSize)
 
-        MenuBarExtra {
-            Text("FlashSpace v\(AppConstants.version)")
-
-            Button("Open") {
-                openWindow(id: "main")
-                NSApp.activate(ignoringOtherApps: true)
-            }
-
-            if settingsRepository.enableSpaceControl {
-                Button("Space Control") {
-                    SpaceControl.show()
-                }
-            }
-
-            Divider()
-
-            Button("Settings") {
-                openWindow(id: "settings")
-                NSApp.activate(ignoringOtherApps: true)
-            }.keyboardShortcut(",")
-
-            Divider()
-
-            Button("Donate") {
-                if let url = URL(string: "https://github.com/sponsors/wojciech-kulik") {
-                    NSWorkspace.shared.open(url)
-                }
-            }
-
-            Button("Project Website") {
-                if let url = URL(string: "https://github.com/wojciech-kulik/FlashSpace") {
-                    NSWorkspace.shared.open(url)
-                }
-            }
-
-            Button("Check for Updates") {
-                Task { await UpdatesManager.shared.showIfNewReleaseAvailable() }
-            }
-
-            Divider()
-
-            Button("Quit") {
-                NSApp.terminate(nil)
-            }.keyboardShortcut("q")
-        } label: {
-            HStack {
-                Image(systemName: workspaceManager.activeWorkspaceDetails?.symbolIconName ?? .defaultIconSymbol)
-                if let title = MenuBarTitle.get() { Text(title) }
-            }
-            .id(settingsRepository.menuBarTitleTemplate)
-        }
+        FlashSpaceMenuBar()
     }
 }
