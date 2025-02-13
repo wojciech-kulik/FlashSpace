9463deefe425e7477dc1d539b20f35b9c87e6a06
Wojciech Kulik <3128467+wojciech-kulik@users.noreply.github.com>
2025-02-13 21:05:39 +0100

    refactor: extracted FlashSpaceMenuBar

---
+++ FlashSpace/Features/MenuBar/FlashSpaceMenuBar.swift
@@ -0,0 +1,69 @@
+//
+//  FlashSpaceMenuBar.swift
+//
+//  Created by Wojciech Kulik on 13/02/2025.
+//  Copyright © 2025 Wojciech Kulik. All rights reserved.
+//
+
+import SwiftUI
+
+struct FlashSpaceMenuBar: Scene {
+    @Environment(\.openWindow) private var openWindow
+
+    @StateObject private var workspaceManager = AppDependencies.shared.workspaceManager
+    @StateObject private var settingsRepository = AppDependencies.shared.settingsRepository
+
+    var body: some Scene {
+        MenuBarExtra {
+            Text("FlashSpace v\(AppConstants.version)")
+
+            Button("Open") {
+                openWindow(id: "main")
+                NSApp.activate(ignoringOtherApps: true)
+            }
+
+            if settingsRepository.enableSpaceControl {
+                Button("Space Control") {
+                    SpaceControl.show()
+                }
+            }
+
+            Divider()
+
+            Button("Settings") {
+                openWindow(id: "settings")
+                NSApp.activate(ignoringOtherApps: true)
+            }.keyboardShortcut(",")
+
+            Divider()
+
+            Button("Donate") {
+                if let url = URL(string: "https://github.com/sponsors/wojciech-kulik") {
+                    NSWorkspace.shared.open(url)
+                }
+            }
+
+            Button("Project Website") {
+                if let url = URL(string: "https://github.com/wojciech-kulik/FlashSpace") {
+                    NSWorkspace.shared.open(url)
+                }
+            }
+
+            Button("Check for Updates") {
+                Task { await UpdatesManager.shared.showIfNewReleaseAvailable() }
+            }
+
+            Divider()
+
+            Button("Quit") {
+                NSApp.terminate(nil)
+            }.keyboardShortcut("q")
+        } label: {
+            HStack {
+                Image(systemName: workspaceManager.activeWorkspaceDetails?.symbolIconName ?? .defaultIconSymbol)
+                if let title = MenuBarTitle.get() { Text(title) }
+            }
+            .id(settingsRepository.menuBarTitleTemplate)
+        }
+    }
+}
