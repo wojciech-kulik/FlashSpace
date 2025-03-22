5d28c9dce8abc696a41fc50b1f47f6f3f9154555
Sergio Patino <sergio@vertigobox.org>
2025-03-22 09:42:46 -0600

    Remove uneeded logic, Update project.yml

--- FlashSpace/Features/Gestures/SwipeManager.swift
+++ FlashSpace/Features/Gestures/SwipeManager.swift
@@ -38,11 +38,10 @@ final class SwipeManager {
 
     static let shared = SwipeManager()
 
-    private var swipeThreshold: Double { workspaceSettings.swipeThreshold }
-    private var naturalDirection: Bool { workspaceSettings.naturalDirection }
-    private var enableSwipeGesture: Bool { workspaceSettings.enableSwipeGesture }
-    private var swipeFingerCount: Int { workspaceSettings.swipeFingerCount.rawValue }
     private var enableWorkspaceTransition: Bool { workspaceSettings.enableWorkspaceTransition }
+    private var naturalDirection: Bool { workspaceSettings.naturalDirection }
+    private var swipeFingerCount: Int { workspaceSettings.swipeFingerCount.rawValue }
+    private var swipeThreshold: Double { workspaceSettings.swipeThreshold }
 
     private var eventTap: CFMachPort?
     private var horizontalSwipeSum: Float = 0
@@ -131,7 +130,7 @@ final class SwipeManager {
 
         if touchesCount == 0 {
             gestureFinished()
-        } else if touchesCount == swipeFingerCount, enableSwipeGesture {
+        } else if touchesCount == swipeFingerCount {
             state = .began
             horizontalSwipeSum += horizontalSwipeDistance(touches: touches)
         }
