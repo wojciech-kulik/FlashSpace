bd1f00c4e578de2d4d58c7146e259211fa064f2d
Wojciech Kulik <3128467+wojciech-kulik@users.noreply.github.com>
2025-03-30 01:12:10 +0100

    feat(gestures)!: fully configurable gestures + added vertical gestures
    Breaking Changes: changed config options for gestures

--- FlashSpace/Features/Settings/Gestures/GesturesSettings.swift
+++ FlashSpace/Features/Settings/Gestures/GesturesSettings.swift
@@ -9,30 +9,30 @@ import Combine
 import Foundation
 
 final class GesturesSettings: ObservableObject {
-    enum FingerCount: Int, CaseIterable, Identifiable {
-        case three = 3
-        case four = 4
-
-        var id: Int { rawValue }
-
-        var description: String {
-            switch self {
-            case .three: return "Three Fingers"
-            case .four: return "Four Fingers"
-            }
-        }
-    }
-
-    @Published var enableSwipeGesture = false {
-        didSet { updateSwipeManager() }
-    }
-
-    @Published var swipeFingerCount: FingerCount = .three {
-        didSet { updateSwipeManager() }
-    }
-
-    @Published var swipeNaturalDirection = false
     @Published var swipeThreshold: Double = 0.2
+    @Published var enableSwipeGestures = false {
+        didSet { updateSwipeManager() }
+    }
+
+    @Published var swipeRight3FingerAction: GestureAction = .nextWorkspace
+    @Published var swipeLeft3FingerAction: GestureAction = .previousWorkspace
+    @Published var swipeRight4FingerAction: GestureAction = .none
+    @Published var swipeLeft4FingerAction: GestureAction = .none
+
+    @Published var swipeUp3FingerAction: GestureAction = .none
+    @Published var swipeDown3FingerAction: GestureAction = .none
+    @Published var swipeUp4FingerAction: GestureAction = .none
+    @Published var swipeDown4FingerAction: GestureAction = .none
+
+    var isVerticalSwipeSet: Bool {
+        swipeUp3FingerAction != .none || swipeDown3FingerAction != .none ||
+            swipeUp4FingerAction != .none || swipeDown4FingerAction != .none
+    }
+
+    var isHorizontalSwipeSet: Bool {
+        swipeLeft3FingerAction != .none || swipeRight3FingerAction != .none ||
+            swipeLeft4FingerAction != .none || swipeRight4FingerAction != .none
+    }
 
     private var observer: AnyCancellable?
     private let updateSubject = PassthroughSubject<(), Never>()
@@ -44,17 +44,25 @@ final class GesturesSettings: ObservableObject {
 
     private func observe() {
         observer = Publishers.MergeMany(
-            $enableSwipeGesture.settingsPublisher(),
-            $swipeFingerCount.settingsPublisher(),
-            $swipeNaturalDirection.settingsPublisher(),
-            $swipeThreshold.settingsPublisher()
+            $enableSwipeGestures.settingsPublisher(),
+            $swipeThreshold.settingsPublisher(),
+
+            $swipeLeft3FingerAction.settingsPublisher(),
+            $swipeRight3FingerAction.settingsPublisher(),
+            $swipeLeft4FingerAction.settingsPublisher(),
+            $swipeRight4FingerAction.settingsPublisher(),
+
+            $swipeUp3FingerAction.settingsPublisher(),
+            $swipeDown3FingerAction.settingsPublisher(),
+            $swipeUp4FingerAction.settingsPublisher(),
+            $swipeDown4FingerAction.settingsPublisher()
         )
         .receive(on: DispatchQueue.main)
         .sink { [weak self] in self?.updateSubject.send() }
     }
 
     private func updateSwipeManager() {
-        if enableSwipeGesture {
+        if enableSwipeGestures {
             SwipeManager.shared.start()
         } else {
             SwipeManager.shared.stop()
@@ -69,17 +77,34 @@ extension GesturesSettings: SettingsProtocol {
 
     func load(from appSettings: AppSettings) {
         observer = nil
-        enableSwipeGesture = appSettings.enableSwipeGesture ?? false
-        swipeFingerCount = appSettings.swipeFingerCount.flatMap(FingerCount.init(rawValue:)) ?? .three
-        swipeNaturalDirection = appSettings.swipeNaturalDirection ?? false
+        enableSwipeGestures = appSettings.enableSwipeGestures ?? false
         swipeThreshold = appSettings.swipeThreshold ?? 0.2
+
+        swipeLeft3FingerAction = appSettings.swipeLeft3FingerAction ?? .previousWorkspace
+        swipeRight3FingerAction = appSettings.swipeRight3FingerAction ?? .nextWorkspace
+        swipeLeft4FingerAction = appSettings.swipeLeft4FingerAction ?? .none
+        swipeRight4FingerAction = appSettings.swipeRight4FingerAction ?? .none
+
+        swipeUp3FingerAction = appSettings.swipeUp3FingerAction ?? .none
+        swipeDown3FingerAction = appSettings.swipeDown3FingerAction ?? .none
+        swipeUp4FingerAction = appSettings.swipeUp4FingerAction ?? .none
+        swipeDown4FingerAction = appSettings.swipeDown4FingerAction ?? .none
+
         observe()
     }
 
     func update(_ appSettings: inout AppSettings) {
-        appSettings.enableSwipeGesture = enableSwipeGesture
-        appSettings.swipeFingerCount = swipeFingerCount.rawValue
-        appSettings.swipeNaturalDirection = swipeNaturalDirection
+        appSettings.enableSwipeGestures = enableSwipeGestures
         appSettings.swipeThreshold = swipeThreshold
+
+        appSettings.swipeLeft3FingerAction = swipeLeft3FingerAction
+        appSettings.swipeRight3FingerAction = swipeRight3FingerAction
+        appSettings.swipeLeft4FingerAction = swipeLeft4FingerAction
+        appSettings.swipeRight4FingerAction = swipeRight4FingerAction
+
+        appSettings.swipeUp3FingerAction = swipeUp3FingerAction
+        appSettings.swipeDown3FingerAction = swipeDown3FingerAction
+        appSettings.swipeUp4FingerAction = swipeUp4FingerAction
+        appSettings.swipeDown4FingerAction = swipeDown4FingerAction
     }
 }
