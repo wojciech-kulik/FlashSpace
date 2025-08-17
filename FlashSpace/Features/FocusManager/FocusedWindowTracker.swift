38542f83f246dda9acbf32c8518b7a51b94ba0c3
Wojciech Kulik <3128467+wojciech-kulik@users.noreply.github.com>
2025-08-17 14:08:36 +0200

    add logs

--- FlashSpace/Features/FocusManager/FocusedWindowTracker.swift
+++ FlashSpace/Features/FocusManager/FocusedWindowTracker.swift
@@ -48,6 +48,8 @@ final class FocusedWindowTracker {
     }
 
     private func activeApplicationChanged(_ app: NSRunningApplication, appLaunch: Bool) {
+        Logger.log("\n\nFouced app changed: \(app.localizedName ?? "Unknown")")
+
         guard appLaunch || settingsRepository.workspaceSettings.activeWorkspaceOnFocusChange else { return }
 
         let activeWorkspaces = workspaceManager.activeWorkspace.values
@@ -73,9 +75,7 @@ final class FocusedWindowTracker {
             app.focusedWindow?.isPictureInPicture(bundleId: app.bundleIdentifier) != true else { return }
 
         let activate = { [self] in
-            Logger.log("")
-            Logger.log("")
-            Logger.log("Activating workspace for app: \(workspace.name)")
+            Logger.log("\n\nActivating workspace for app: \(workspace.name)")
             workspaceManager.updateLastFocusedApp(app.toMacApp, in: workspace)
             workspaceManager.activateWorkspace(workspace, setFocus: false)
             app.activate()
@@ -85,6 +85,8 @@ final class FocusedWindowTracker {
                 pictureInPictureManager.restoreAppIfNeeded(app: app)
             }
         }
+
+        Logger.log("Activating workspace based on focuse change for app: \(app.localizedName ?? "Unknown")")
 
         if workspace.isDynamic, workspace.displays.isEmpty {
             DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500)) {
--- FlashSpace/Features/Workspaces/WorkspaceManager.swift
+++ FlashSpace/Features/Workspaces/WorkspaceManager.swift
@@ -168,6 +168,7 @@ final class WorkspaceManager: ObservableObject {
     }
 
     private func hideApps(in workspace: Workspace) {
+        Logger.log("Hiding apps for workspace: \(workspace.name)")
         let regularApps = NSWorkspace.shared.runningApplications
             .filter { $0.activationPolicy == .regular }
         let workspaceApps = workspace.apps + floatingAppsSettings.floatingApps
@@ -443,6 +444,8 @@ extension WorkspaceManager {
     }
 
     func activateWorkspace(next: Bool, skipEmpty: Bool, loop: Bool) {
+        Logger.log("Cycle: Requested to activate \(next ? "next" : "previous") workspace")
+
         guard let screen = displayManager.getCursorScreen() else { return }
 
         var screenWorkspaces = workspaceRepository.workspaces
@@ -473,6 +476,7 @@ extension WorkspaceManager {
 
         guard let selectedWorkspace, selectedWorkspace.id != activeWorkspace.id else { return }
 
+        Logger.log("Cycle: activating workspace \(selectedWorkspace.name)")
         activateWorkspace(selectedWorkspace, setFocus: true)
     }
 
@@ -481,6 +485,7 @@ extension WorkspaceManager {
               let mostRecentWorkspace = mostRecentWorkspace[screen]
         else { return }
 
+        Logger.log("activateRecentWorkspace \(mostRecentWorkspace.name)")
         activateWorkspace(mostRecentWorkspace, setFocus: true)
     }
 
@@ -488,6 +493,7 @@ extension WorkspaceManager {
         guard activeWorkspace.values.contains(where: { $0.id == workspaceId }) else { return }
         guard let updatedWorkspace = workspaceRepository.workspaces.first(where: { $0.id == workspaceId }) else { return }
 
+        Logger.log("activateWorkspaceIfActive \(updatedWorkspace.name)")
         activateWorkspace(updatedWorkspace, setFocus: false)
     }
 
